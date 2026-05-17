9b56e4ce644e49fa79dd8e11c23204a73a0d5c2e
byronrich <alex.richr@gmail.com>
2026-05-16 22:27:52 -0600

    server.js fix contacts part 2

--- server.js
+++ server.js
@@ -4,15 +4,24 @@ import dotenv from "dotenv";
 import contactsRoutes from "./routes/contacts.js";
 import { connectDB } from "./db/connect.js";
 
-// Swagger imports
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "./swagger.json" assert { type: "json" };
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Fix for loading swagger.json in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "swagger.json"), "utf8")
+);
 
 // Swagger route
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
